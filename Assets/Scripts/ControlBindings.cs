01fb807f509e0e74e4044700f9dd242fd2bc3323
aureliokessler <aureliokessler@gmail.com>
2022-01-07 20:15:31 +0100

    copy the StarterAsssetsinputs.cs from the Starter Assets Third Person Controller

--- Assets/Scripts/ControlBindings.cs
+++ Assets/Scripts/ControlBindings.cs
@@ -1,55 +1,88 @@
 using UnityEngine;
+#if ENABLE_INPUT_SYSTEM && STARTER_ASSETS_PACKAGES_CHECKED
 using UnityEngine.InputSystem;
+#endif
 
-public class ControlBindings : MonoBehaviour
+namespace StarterAssets
 {
-    [Header("Character Input Values")]
-    public Vector2 move;
-    public Vector2 look;
-    public bool jump;
-    public bool sprint;
+    public class StarterAssetsInputs : MonoBehaviour
+    {
+        [Header("Character Input Values")]
+        public Vector2 move;
+        public Vector2 look;
+        public bool jump;
+        public bool sprint;
 
-    [Header("Movement Settings")]
-    public bool analogMovement;
-    
+        [Header("Movement Settings")]
+        public bool analogMovement;
+
 #if !UNITY_IOS || !UNITY_ANDROID
-    [Header("Mouse Cursor Settings")]
-    public bool cursorLocked = true;
-    public bool cursorInputForLook = true;
+        [Header("Mouse Cursor Settings")]
+        public bool cursorLocked = true;
+        public bool cursorInputForLook = true;
 #endif
 
-    public void Move(InputValue value)
-    {
-        move = value.Get<Vector2>();
-    }
-    
-    public void Look(InputValue value)
-    {
-        if (cursorInputForLook)
+#if ENABLE_INPUT_SYSTEM && STARTER_ASSETS_PACKAGES_CHECKED
+		public void OnMove(InputValue value)
+		{
+			MoveInput(value.Get<Vector2>());
+		}
+
+		public void OnLook(InputValue value)
+		{
+			if(cursorInputForLook)
+			{
+				LookInput(value.Get<Vector2>());
+			}
+		}
+
+		public void OnJump(InputValue value)
+		{
+			JumpInput(value.isPressed);
+		}
+
+		public void OnSprint(InputValue value)
+		{
+			SprintInput(value.isPressed);
+		}
+#else
+        // old input sys if we do decide to have it (most likely wont)...
+#endif
+
+        public void MoveInput(Vector2 newMoveDirection)
         {
-            look = value.Get<Vector2>();
-        }
-    }
-    
-    public void Jump(InputValue value)
-    {
-        jump = value.isPressed;
-    }
-    
-    public void Sprint(InputValue value)
-    {
-        sprint = value.isPressed;
-    }
-    
-#if !UNITY_IOS || !UNITY_ANDROID
-    private void OnApplicationFocus(bool hasFocus)
-    {
-        SetCursorState(cursorLocked);
-    }
+            move = newMoveDirection;
+        } 
 
-    private void SetCursorState(bool newState)
-    {
-        Cursor.lockState = newState ? CursorLockMode.Locked : CursorLockMode.None;
-    }
+        public void LookInput(Vector2 newLookDirection)
+        {
+            look = newLookDirection;
+        }
+
+        public void JumpInput(bool newJumpState)
+        {
+            jump = newJumpState;
+        }
+
+        public void SprintInput(bool newSprintState)
+        {
+            sprint = newSprintState;
+        }
+
+#if !UNITY_IOS || !UNITY_ANDROID
+
+        private void OnApplicationFocus(bool hasFocus)
+        {
+            SetCursorState(cursorLocked);
+        }
+
+        private void SetCursorState(bool newState)
+        {
+            Cursor.lockState = newState ? CursorLockMode.Locked : CursorLockMode.None;
+        }
+
 #endif
+
+    }
+	
 }
